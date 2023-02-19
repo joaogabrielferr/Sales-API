ab8e4eeb10034e4d9c0df88d35ea13d18a3ec9e8
Joao Gabriel <joaogabrielferr@gmail.com>
2023-02-19 19:44:58 -0300

    feat: adding product routes

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,6 +1,9 @@
 import {Request, Response, Router} from 'express';
+import productsRouter from 'src/modules/products/Routes/product.routes';
 
 const routes = Router();
+
+routes.use('/products',productsRouter);
 
 routes.get('/',(request : Request,response : Response)=>{
     return response.json({message: "Hello World"});
