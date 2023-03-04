b66c56157f5d3e996dea3bdb23a2bf0876da6bc8
Joao Gabriel <joaogabrielferr@gmail.com>
2023-03-04 20:07:17 -0300

    feat: adding encryption to passwords before storing them in db

--- src/modules/users/services/CreateUserService.ts
+++ src/modules/users/services/CreateUserService.ts
@@ -1,4 +1,5 @@
 import AppError from "@shared/errors/AppError";
+import { hash } from "bcryptjs";
 import { getCustomRepository } from "typeorm";
 import User from "../typeorm/entities/User";
 import { UserRepository } from "../typeorm/repositories/UserRepository";
@@ -24,10 +25,12 @@ export default class CreateUserService{
       throw new AppError('There is already a user with this email');
     }
 
+    const hashedPassword = await hash(password,8);
+
     const user = userRepo.create({
       name,
       email,
-      password
+      password : hashedPassword
     });
 
     await userRepo.save(user);
