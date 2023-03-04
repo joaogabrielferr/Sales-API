cc03d8efa48d8dec0c2a470434563d05c22f73c6
Joao Gabriel <joaogabrielferr@gmail.com>
2023-03-04 19:15:30 -0300

    feat: add user controller and user routes

---
+++ src/modules/users/routes/user.routes.ts
@@ -0,0 +1,20 @@
+import { Router } from "express";
+import {celebrate,Joi,Segments} from 'celebrate';
+import UsersController from "../controllers/UsersController";
+
+
+const userRouter = Router();
+
+const userController = new UsersController();
+
+userRouter.get('/',userController.index);
+
+userRouter.post('/user',
+celebrate({
+  [Segments.BODY] :{
+    name : Joi.string().required(),
+    email : Joi.string().email().required(),
+    password : Joi.string().required()
+  }
+})
+,userController.create);
