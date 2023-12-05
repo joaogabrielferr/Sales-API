a596c96d0cdb61999c3cae850fa1f73cddeb4f73
Gabriel <joaogabrielferr@gmail.com>
2023-12-05 17:41:07 -0300

    feat: adding middleware to handle errors

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 import routes from './routes';
+import AppError from '@shared/errors/AppError';
 
 const app = express();
 app.use(cors());
@@ -8,7 +9,18 @@ app.use(express.json());
 
 app.use(routes);
 
-app.use((error: Error, req: Request, res: Response, next: NextFunction) => {});
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+  if (error instanceof AppError) {
+    return res.status(error.statusCode).json({
+      status: 'error',
+      message: error.message,
+    });
+  }
+  return res.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+});
 
 app.listen(3333, () => {
   console.log('server running on port 3333');
