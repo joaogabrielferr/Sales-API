f753c28b4119638627e70e1c17193a04d538a08c
Gabriel <joaogabrielferr@gmail.com>
2023-12-05 17:37:47 -0300

    feat: configuring error handling

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 import routes from './routes';
 
@@ -7,6 +7,8 @@ app.use(cors());
 app.use(express.json());
 
 app.use(routes);
+
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {});
 
 app.listen(3333, () => {
   console.log('server running on port 3333');
