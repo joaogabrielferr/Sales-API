90ee2ca4af8b487d6ad520e955448a7e439fa4fa
Gabriel <joaogabrielferr@gmail.com>
2023-12-06 20:55:12 -0300

    adding validation for product requests

--- src/modules/products/routes/ProductRoutes.ts
+++ src/modules/products/routes/ProductRoutes.ts
@@ -1,14 +1,58 @@
 import { Router } from 'express';
 import ProductController from '../controllers/ProductController';
+import { celebrate, Joi, Segments } from 'celebrate';
 
 const productRouter = Router();
 
 const productController = new ProductController();
 
 productRouter.get('/', productController.index);
-productRouter.get('/:id', productController.show);
-productRouter.post('/', productController.create);
-productRouter.put('/:id', productController.update);
-productRouter.delete('/:id', productController.delete);
+
+productRouter.get(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+  }),
+  productController.show,
+);
+
+productRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      price: Joi.number().precision(2).required(),
+      quantity: Joi.number().required(),
+    },
+  }),
+  productController.create,
+);
+
+productRouter.put(
+  '/:id',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      price: Joi.number().precision(2).required(),
+      quantity: Joi.number().required(),
+    },
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+  }),
+  productController.update,
+);
+
+productRouter.delete(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+  }),
+  productController.delete,
+);
 
 export default productRouter;
--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,16 +1,21 @@
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
+import 'reflect-metadata';
 import cors from 'cors';
 import routes from './routes';
 import AppError from '@shared/errors/AppError';
-import 'reflect-metadata';
 import '@shared/typeorm';
+import { errors } from 'celebrate';
 
 const app = express();
+
 app.use(cors());
+
 app.use(express.json());
 
 app.use(routes);
+
+app.use(errors());
 
 app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
   if (error instanceof AppError) {
